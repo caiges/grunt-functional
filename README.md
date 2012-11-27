3a4efef6b7bb5265a7c3c93990264d7869387f0a
caiges <caigesn@gmail.com>
2012-11-27 11:14:59 -0700

    Updated readme and removed boilerplate grunt plugin cruft.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # grunt-functional
 
-Run your functional tests through casperjs.
+Run self-hosted functional tests through casperjs.
 
 ## Getting Started
 Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-functional`
@@ -15,14 +15,22 @@ grunt.loadNpmTasks('grunt-functional');
 [getting_started]: https://github.com/gruntjs/grunt/blob/master/docs/getting_started.md
 
 ## Documentation
-_(Coming soon)_
 
-## Contributing
-In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt][grunt].
+1. [Install CasperJS](http://casperjs.org/installation.html)
+2. Add your functional tests to your project's "test/functional" directory.
+3. Add the following to your configuration:
+
+```javascript
+functional_tests: {
+  hostname: 'localhost',
+  port: 35729,
+}
+```
+4. Run `grunt functional` and your functional tests will run on the host and port specified in your configuration. 
 
 ## Release History
-_(Nothing yet)_
+0.1.0 - Initial Release.
 
 ## License
-Copyright (c) 2012 caiges  
+Copyright (c) 2012 Caige Nichols "caiges"  
 Licensed under the MIT license.
--- tasks/functional.js
+++ tasks/functional.js
@@ -8,23 +8,38 @@
 
 module.exports = function(grunt) {
 
-  // Please see the grunt documentation for more information regarding task and
-  // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
+  // Run our functional test suite using phantomjs
+  grunt.registerTask('functional', 'Run functional tests with phantomjs + casperjs', function() {
+    var done = this.async();
+    // Options for the server hosting our app
+    var opts = {
+      open: false,
+      port: grunt.config('functional_tests.port') || 35729,
+      base: 'app',
+      // We don't need the livereload injection middleware
+      inject: false,
+      target: 'phantom',
+      hostname: grunt.config('functional_tests.hostname') || 'localhost'
+    };
 
-  // ==========================================================================
-  // TASKS
-  // ==========================================================================
+    // Spawn the casperjs yeoman task in a child process and then close out the server/end this task
+    function runTests() {
+      grunt.utils.spawn({cmd: 'yeoman', args: ['casperjs']}, closeServer);
+    }
 
-  grunt.registerTask('functional', 'Your task description goes here.', function() {
-    grunt.log.write(grunt.helper('functional'));
-  });
+    // Start a server hosting our app
+    var server = grunt.helper('server', opts, runTests);
 
-  // ==========================================================================
-  // HELPERS
-  // ==========================================================================
-
-  grunt.registerHelper('functional', function() {
-    return 'functional!!!';
+    function closeServer(error, result, code) {
+      if(error) {
+        grunt.log.writeln(error);
+      }
+      grunt.log.writeln(result);
+      // Close the server
+      server.close();
+      // End the task
+      done(true);
+    }
   });
 
 };
