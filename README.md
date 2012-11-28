f94e215a324ddc39889ca963aff7dfa394a06e44
caiges <caigesn@gmail.com>
2012-11-28 00:04:01 -0700

    Fixed grunt/yeoman compatibility. Yeoman is using an alpha version of grunt which expected Gruntfile.js. Added a server provider that doesn't depend on Yeoman.

--- README.md
+++ README.md
@@ -5,13 +5,9 @@ Run self-hosted functional tests through casperjs.
 This task will spin up your app on the host and port specified in your configuration and run your functional tests.
 
 ## Getting Started
-Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-functional`
+
 
 Then add this line to your project's `grunt.js` gruntfile:
-
-```javascript
-grunt.loadNpmTasks('grunt-functional');
-```
 
 [grunt]: http://gruntjs.com/
 [getting_started]: https://github.com/gruntjs/grunt/blob/master/docs/getting_started.md
@@ -19,26 +15,36 @@ grunt.loadNpmTasks('grunt-functional');
 ## Documentation
 
 1. [Install CasperJS](http://casperjs.org/installation.html)
-2. [Install and configure grunt-casperjs](https://github.com/ronaldlokers/grunt-casperjs)
+2. Install and configure [grunt-casperjs](https://github.com/ronaldlokers/grunt-casperjs) with: `npm install grunt-casperjs`
 
     ```javascript
       casperjs: {
         files: ['test/functional/**/*.js']
       }
     ```
-3. Add your functional tests to your project's "test/functional" directory.
-4. Add the following to your configuration:
+3. Install and configure grunt-functional with: `npm install grunt-functional`
 
     ```javascript
+      grunt.loadNpmTasks('grunt-functional');
+    ```
+4. Add your functional tests to your project's "test/functional" directory.
+5. Add the following to your configuration:
+
+    ```javascript
+      server: {
+        base: 'app'
+      },
       functional_tests: {
         hostname: 'localhost',
         port: 35729,
       }
     ```
-5. Run `grunt functional` and your functional tests will run on the host and port specified in your configuration. 
+
+6. Run `grunt functional` and your functional tests will run on the host and port specified in your configuration.
 
 ## Release History
-0.1.1 - Initial Release.
+- 0.2.0 - Fixed grunt/yeoman compatibility. Yeoman is using an alpha version of grunt which expected Gruntfile.js. Added a server provider that doesn't depend on Yeoman.
+- 0.1.1 - Initial Release.
 
 ## License
 Copyright (c) 2012 Caige Nichols "caiges"  
--- tasks/functional.js
+++ tasks/functional.js
@@ -2,9 +2,13 @@
  * grunt-functional
  * http://caiges.github.com/grunt-functional
  *
- * Copyright (c) 2012 caiges
+ * Copyright (c) 2012 Caige Nichols "caiges"
  * Licensed under the MIT license.
  */
+
+var connect = require('connect'),
+    fs = require('fs'),
+    path = require('path');
 
 module.exports = function(grunt) {
 
@@ -13,33 +17,66 @@ module.exports = function(grunt) {
     var done = this.async();
     // Options for the server hosting our app
     var opts = {
-      open: false,
-      port: grunt.config('functional_tests.port') || 35729,
-      base: 'app',
-      // We don't need the livereload injection middleware
-      inject: false,
-      target: 'phantom',
-      hostname: grunt.config('functional_tests.hostname') || 'localhost'
+      base: grunt.config('server.base'),
+      hostname: grunt.config('functional_tests.hostname') || 'localhost',
+      port: grunt.config('functional_tests.port') || 35729
     };
 
-    // Spawn the casperjs yeoman task in a child process and then close out the server/end this task
+    // Spawn the casperjs grunt task in a child process and then close out the server/end this task.
+    // Note: Yeoman uses an alpha version of grunt which expects Grunfile.js instead of grunt.js.
     function runTests() {
-      grunt.utils.spawn({cmd: 'yeoman', args: ['casperjs']}, closeServer);
+      fs.exists('Gruntfile.js', function(exists) {
+        if(exists) {
+          grunt.utils.spawn({cmd: 'grunt', args: ['--config', 'Gruntfile.js', 'casperjs']}, closeServer);
+        } else {
+          grunt.utils.spawn({cmd: 'grunt', args: ['casperjs']}, closeServer);
+        }
+      });
     }
 
     // Start a server hosting our app
-    var server = grunt.helper('server', opts, runTests);
+    var server = grunt.helper('functional', opts, runTests);
 
     function closeServer(error, result, code) {
       if(error) {
         grunt.log.writeln(error);
       }
       grunt.log.writeln(result);
+
       // Close the server
       server.close();
       // End the task
       done(true);
     }
+  });
+
+grunt.registerHelper('functional', function(opts, cb) {
+    cb = cb || function() {};
+    console.log(cb);
+    var middleware = [];
+
+    // Serve static files.
+    middleware.push(connect.static(opts.base));
+
+    return connect.apply(null, middleware)
+      .on('error', function( err ) {
+        if ( err.code === 'EADDRINUSE' ) {
+          return this.listen(0); // 0 means random port
+        }
+
+        // not an EADDRINUSE error, buble up the error
+        cb(err);
+      })
+      .listen(opts.port, function() {
+        var port = this.address().port;
+
+        // Start server.
+        grunt.log
+          .subhead( 'Starting static web server on port '.yellow + String( port ).red )
+          .writeln( '  - ' + path.resolve(opts.base) );
+
+        cb(null, port);
+      });
   });
 
 };
